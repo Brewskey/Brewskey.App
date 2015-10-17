18d654b110a7b8cae4ec6a3341039b28eafbcabc
john kalberer <jlkalberer@gmail.com>
2015-10-16 20:33:04 -0700

    Updated ionic and added android.

--- www/js/services/nfc.service.js
+++ www/js/services/nfc.service.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
 angular.module('tappt.services')
-.factory("nfc",
+.factory("nfcService",
   ['$q', '$ionicPopup', '$state', 'Restangular',
   function ($q, $ionicPopup, $state, rest) {
       var requestPour = false;
