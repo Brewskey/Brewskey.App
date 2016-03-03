62865bd54046d3a2262e9f90c0c761e848348fec
John Kalberer <jlkalberer@gmail.com>
2016-03-03 08:36:02 -0800

    Updated load fail directive so it doesn't re-run the error code which rebinds the image source.

--- www/js/directives/image-load-fail.directive.js
+++ www/js/directives/image-load-fail.directive.js
@@ -5,6 +5,7 @@ function () {
         link: function (scope, iElement, attrs) {
             iElement.bind('error', function () {
                 angular.element(this).attr("src", attrs.loadFail);
+                iElement.unbind('error');
             });
         }
     };
