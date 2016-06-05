5d2168c029d2e100dd14ef66266697c16cc94951
John kalberer <jlkalberer@gmail.com>
2016-06-05 14:33:47 -0700

    Updates to pour dialog.

---
+++ www/js/directives/limit-to.directive.js
@@ -0,0 +1,12 @@
+﻿angular.module('brewskey.directives')
+.directive("limitTo", [function () {
+    return {
+        restrict: "A",
+        link: function (scope, elem, attrs) {
+            var limit = parseInt(attrs.limitTo);
+            angular.element(elem).on("keypress", function (e) {
+                if (this.value.length == limit) e.preventDefault();
+            });
+        }
+    }
+}]);
