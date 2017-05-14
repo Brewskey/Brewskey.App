d0ef94b35d32b3383e6a46e16ccd4043e9901867
Anton puko <antonpukowebdev@gmail.com>
2017-05-14 23:20:12 +0200

    implement days picker

---
+++ www/js/directives/days-picker.directive.js
@@ -0,0 +1,31 @@
+angular.module('brewskey.directives').directive('daysPicker', [
+  'scheduleUtils',
+  function(scheduleUtils) {
+    return {
+      link: function(scope) {
+        scope.days = scheduleUtils.DAYS;
+
+        scope.$watch('daysCombinedFlag', function(daysCombinedFlag) {
+          scope.selectedDaysMap = _.chain(scheduleUtils.DAYS)
+            .filter(function(day) {
+              return scheduleUtils.isDaySelected(daysCombinedFlag, day.name);
+            })
+            .keyBy('name')
+            .value();
+        });
+
+        scope.toggleDay = function(day) {
+          scope.daysCombinedFlag =
+            scope.daysCombinedFlag ^
+            scheduleUtils.SCHEDULE_DAY_BIT_MAP[day.name];
+        };
+      },
+      restrict: 'E',
+      scope: {
+        daysCombinedFlag: '=',
+        isDisabled: '<',
+      },
+      templateUrl: 'templates/directives/days-picker.html',
+    };
+  },
+]);
