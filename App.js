e15ee6d840840896051c9ae37021938da72ad596
Anton puko <antonpukowebdev@gmail.com>
2017-09-30 14:07:58 +0200

    add mobx, update react dep

--- App.js
+++ App.js
@@ -2,12 +2,24 @@
 
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { observer } from 'mobx-react';
+import { action, observable } from 'mobx';
 
+@observer
 export default class App extends React.Component {
+  @observable counter = 0;
+
+  @action increment = () => this.counter++;
+
+  componentDidMount() {
+    setInterval(this.increment, 1000);
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>brewskey app hello world</Text>
+        <Text>brewskey supper dupa app</Text>
+        <Text>Counter: {this.counter}</Text>
       </View>
     );
   }
