9c01ab5ae8162226e0ce5018c37ba0e5e081b810
Anton puko <antonpukowebdev@gmail.com>
2017-09-30 03:19:39 +0200

    init react-native project

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>hello world i'm here</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fb3',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
