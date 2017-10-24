3741c2aa759ca966861880b83da3a1a6d32a4eb8
Anton puko <antonpukowebdev@gmail.com>
2017-10-24 18:19:05 +0200

    add isOrganic checkbox field to beverage form, fix picker component name

--- src/components/BeverageForm.js
+++ src/components/BeverageForm.js
@@ -16,9 +16,9 @@ import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view
 import { Button, FormValidationMessage } from 'react-native-elements';
 import Form from '../common/form/Form';
 import FormField from '../common/form/FormField';
-
+import CheckBoxField from './CheckBoxField';
 import TextField from './TextField';
-import Picker from './Picker';
+import PickerField from './PickerField';
 
 const YEARS_RANGE_LENGTH = 10;
 
@@ -53,8 +53,7 @@ class BeverageForm extends React.Component<Props> {
   }
 
   // todo Implement custom component for srm
-  // todo implement autocomplete for style
-  // todo add isOrganic field when taps pr will be merged(need CheckboxField component)
+  // todo implement autocomplete for style?
   render(): React.Node {
     const { beverage, onSubmit, submitButtonLabel } = this.props;
     const currentYear = new Date().getFullYear();
@@ -96,12 +95,12 @@ class BeverageForm extends React.Component<Props> {
             </FormField>
             <FormField initialValue={beverage.beverageType} name="beverageType">
               {(formFieldProps: FormFieldChildProps): React.Node => (
-                <Picker label="Type" {...formFieldProps}>
-                  <Picker.Item label="Beer" value="Beer" />
-                  <Picker.Item label="Cider" value="Cider" />
-                  <Picker.Item label="Coffee" value="Coffee" />
-                  <Picker.Item label="Soda" value="Soda" />
-                </Picker>
+                <PickerField label="Type" {...formFieldProps}>
+                  <PickerField.Item label="Beer" value="Beer" />
+                  <PickerField.Item label="Cider" value="Cider" />
+                  <PickerField.Item label="Coffee" value="Coffee" />
+                  <PickerField.Item label="Soda" value="Soda" />
+                </PickerField>
               )}
             </FormField>
             <FormField
@@ -109,72 +108,81 @@ class BeverageForm extends React.Component<Props> {
               name="servingTemperature"
             >
               {(formFieldProps: FormFieldChildProps): React.Node => (
-                <Picker label="Serving Temperature" {...formFieldProps}>
-                  <Picker.Item label="Cellar" value="cellar" />
-                  <Picker.Item label="Cold" value="cold" />
-                  <Picker.Item label="Cool" value="cool" />
-                  <Picker.Item label="Hot" value="hot" />
-                  <Picker.Item label="Very Cold" value="very_cold" />
-                  <Picker.Item label="Warm" value="warm" />
-                </Picker>
+                <PickerField label="Serving Temperature" {...formFieldProps}>
+                  <PickerField.Item label="Cellar" value="cellar" />
+                  <PickerField.Item label="Cold" value="cold" />
+                  <PickerField.Item label="Cool" value="cool" />
+                  <PickerField.Item label="Hot" value="hot" />
+                  <PickerField.Item label="Very Cold" value="very_cold" />
+                  <PickerField.Item label="Warm" value="warm" />
+                </PickerField>
               )}
             </FormField>
             <FormField initialValue={beverage.year} name="year">
               {(formFieldProps: FormFieldChildProps): React.Node => (
-                <Picker label="Year" {...formFieldProps}>
+                <PickerField label="Year" {...formFieldProps}>
                   {yearsRange.map((year: number): React.Node => (
-                    <Picker.Item
+                    <PickerField.Item
                       key={year}
                       label={year.toString()}
                       value={year.toString()}
                     />
                   ))}
-                </Picker>
+                </PickerField>
               )}
             </FormField>
             <FormField initialValue={beverage.availability} name="availability">
               {(formFieldProps: FormFieldChildProps): React.Node => (
-                <Picker label="Availability" {...formFieldProps}>
+                <PickerField label="Availability" {...formFieldProps}>
                   {this.props.availabilityStore.all.map(
                     ({ id, name }: Availability): React.Node => (
-                      <Picker.Item key={id} label={name} value={id} />
+                      <PickerField.Item key={id} label={name} value={id} />
                     ),
                   )}
-                </Picker>
+                </PickerField>
               )}
             </FormField>
             <FormField initialValue={beverage.glassware} name="glassware">
               {(formFieldProps: FormFieldChildProps): React.Node => (
-                <Picker label="Glass" {...formFieldProps}>
+                <PickerField label="Glass" {...formFieldProps}>
                   {this.props.glassStore.all.map(
                     ({ id, name }: Glass): React.Node => (
-                      <Picker.Item key={id} label={name} value={id} />
+                      <PickerField.Item key={id} label={name} value={id} />
                     ),
                   )}
-                </Picker>
+                </PickerField>
+              )}
+            </FormField>
+            <FormField initialValue={beverage.isOrganic} name="isOrganic">
+              {(formFieldProps: FormFieldChildProps): React.Node => (
+                <CheckBoxField
+                  disabled={submitting}
+                  label="Is Organic?"
+                  {...formFieldProps}
+                />
               )}
             </FormField>
             {values.beverageType === 'Beer' && [
               <FormField initialValue={beverage.srm} key="srm" name="srm">
                 {(formFieldProps: FormFieldChildProps): React.Node => (
-                  <Picker label="Srm" {...formFieldProps}>
+                  <PickerField label="Srm" {...formFieldProps}>
                     {this.props.srmStore.all.map(
                       ({ id, name }: Srm): React.Node => (
-                        <Picker.Item key={id} label={name} value={id} />
+                        <PickerField.Item key={id} label={name} value={id} />
                       ),
                     )}
-                  </Picker>
+                  </PickerField>
                 )}
               </FormField>,
               <FormField initialValue={beverage.style} key="style" name="style">
                 {(formFieldProps: FormFieldChildProps): React.Node => (
-                  <Picker label="Style" {...formFieldProps}>
+                  <PickerField label="Style" {...formFieldProps}>
                     {this.props.styleStore.all.map(
                       ({ id, name }: Style): React.Node => (
-                        <Picker.Item key={id} label={name} value={id} />
+                        <PickerField.Item key={id} label={name} value={id} />
                       ),
                     )}
-                  </Picker>
+                  </PickerField>
                 )}
               </FormField>,
               <FormField initialValue={beverage.abv} key="abv" name="abv">
