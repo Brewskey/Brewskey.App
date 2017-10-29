11d7e9e791393afe63e230c1dc9e24da1d4d4558
Anton puko <antonpukowebdev@gmail.com>
2017-10-29 02:59:31 +0200

    imlement experimental FieldFocusManager

--- src/components/LocationForm/index.js
+++ src/components/LocationForm/index.js
@@ -3,6 +3,7 @@
 import type {
   FormChildProps,
   FormFieldChildProps,
+  FieldFocusManagerChildProps,
 } from '../../common/form/types';
 import type { StateConfig } from './stateList';
 
@@ -14,6 +15,7 @@ import { Button, FormValidationMessage } from 'react-native-elements';
 import STATE_LIST from './stateList';
 import Form from '../../common/form/Form';
 import FormField from '../../common/form/FormField';
+import FieldFocusManager from '../../common/form/FieldFocusManager';
 import TextField from '../TextField';
 import PickerField from '../PickerField';
 
@@ -65,98 +67,126 @@ const LocationForm = observer(
         pristine,
         submitting,
       }: FormChildProps): React.Node => (
-        <KeyboardAwareScrollView style={styles.container}>
-          <FormField initialValue={location.name} name="name">
-            {(formFieldProps: FormFieldChildProps): React.Node => (
-              <TextField
-                returnKeyType="next"
-                disabled={submitting}
-                label="Name"
-                {...formFieldProps}
+        <FieldFocusManager>
+          {({
+            addFieldRef,
+            focusField,
+          }: FieldFocusManagerChildProps): React.Node => (
+            <KeyboardAwareScrollView style={styles.container}>
+              <FormField initialValue={location.name} name="name">
+                {(formFieldProps: FormFieldChildProps): React.Node => (
+                  <TextField
+                    disabled={submitting}
+                    inputRef={addFieldRef(0)}
+                    label="Name"
+                    onSubmitEditing={focusField(1)}
+                    returnKeyType="next"
+                    {...formFieldProps}
+                  />
+                )}
+              </FormField>
+              <FormField initialValue={location.summary} name="summary">
+                {(formFieldProps: FormFieldChildProps): React.Node => (
+                  <TextField
+                    disabled={submitting}
+                    inputRef={addFieldRef(1)}
+                    label="Summary"
+                    onSubmitEditing={focusField(2)}
+                    returnKeyType="next"
+                    {...formFieldProps}
+                  />
+                )}
+              </FormField>
+              <FormField initialValue={location.description} name="description">
+                {(formFieldProps: FormFieldChildProps): React.Node => (
+                  <TextField
+                    disabled={submitting}
+                    inputRef={addFieldRef(2)}
+                    label="Description"
+                    returnKeyType="next"
+                    {...formFieldProps}
+                  />
+                )}
+              </FormField>
+              <FormField
+                initialValue={location.locationType}
+                name="locationType"
+              >
+                {(formFieldProps: FormFieldChildProps): React.Node => (
+                  <PickerField label="Location type" {...formFieldProps}>
+                    <PickerField.Item label="Kegerator" value="Kegerator" />
+                    <PickerField.Item label="Bar" value="Bar" />
+                  </PickerField>
+                )}
+              </FormField>
+              <FormField initialValue={location.street} name="street">
+                {(formFieldProps: FormFieldChildProps): React.Node => (
+                  <TextField
+                    disabled={submitting}
+                    inputRef={addFieldRef(3)}
+                    label="Street address"
+                    onSubmitEditing={focusField(4)}
+                    returnKeyType="next"
+                    {...formFieldProps}
+                  />
+                )}
+              </FormField>
+              <FormField initialValue={location.suite} name="suite">
+                {(formFieldProps: FormFieldChildProps): React.Node => (
+                  <TextField
+                    disabled={submitting}
+                    inputRef={addFieldRef(4)}
+                    label="Apt./Suite"
+                    onSubmitEditing={focusField(5)}
+                    returnKeyType="next"
+                    {...formFieldProps}
+                  />
+                )}
+              </FormField>
+              <FormField initialValue={location.city} name="city">
+                {(formFieldProps: FormFieldChildProps): React.Node => (
+                  <TextField
+                    disabled={submitting}
+                    inputRef={addFieldRef(5)}
+                    label="City"
+                    returnKeyType="next"
+                    {...formFieldProps}
+                  />
+                )}
+              </FormField>
+              <FormField initialValue={location.state} name="state">
+                {(formFieldProps: FormFieldChildProps): React.Node => (
+                  <PickerField label="State" {...formFieldProps}>
+                    {STATE_LIST.map((stateConfig: StateConfig): React.Node => (
+                      <PickerField.Item
+                        key={stateConfig.value}
+                        {...stateConfig}
+                      />
+                    ))}
+                  </PickerField>
+                )}
+              </FormField>
+              <FormField initialValue={location.zipCode} name="zipCode">
+                {(formFieldProps: FormFieldChildProps): React.Node => (
+                  <TextField
+                    disabled={submitting}
+                    inputRef={addFieldRef(6)}
+                    keyboardType="numeric"
+                    label="Zip"
+                    {...formFieldProps}
+                  />
+                )}
+              </FormField>
+              <FormField name="id" initialValue={location.id} />
+              <FormValidationMessage>{formError}</FormValidationMessage>
+              <Button
+                disabled={submitting || invalid || pristine}
+                onPress={(): void => handleSubmit(onSubmit)}
+                title={submitButtonLabel}
               />
-            )}
-          </FormField>
-          <FormField initialValue={location.summary} name="summary">
-            {(formFieldProps: FormFieldChildProps): React.Node => (
-              <TextField
-                returnKeyType="next"
-                disabled={submitting}
-                label="Summary"
-                {...formFieldProps}
-              />
-            )}
-          </FormField>
-          <FormField initialValue={location.description} name="description">
-            {(formFieldProps: FormFieldChildProps): React.Node => (
-              <TextField
-                disabled={submitting}
-                label="Description"
-                {...formFieldProps}
-              />
-            )}
-          </FormField>
-          <FormField initialValue={location.locationType} name="locationType">
-            {(formFieldProps: FormFieldChildProps): React.Node => (
-              <PickerField label="Location type" {...formFieldProps}>
-                <PickerField.Item label="Kegerator" value="Kegerator" />
-                <PickerField.Item label="Bar" value="Bar" />
-              </PickerField>
-            )}
-          </FormField>
-          <FormField initialValue={location.street} name="street">
-            {(formFieldProps: FormFieldChildProps): React.Node => (
-              <TextField
-                disabled={submitting}
-                label="Street address"
-                {...formFieldProps}
-              />
-            )}
-          </FormField>
-          <FormField initialValue={location.suite} name="suite">
-            {(formFieldProps: FormFieldChildProps): React.Node => (
-              <TextField
-                disabled={submitting}
-                label="Apt./Suite"
-                {...formFieldProps}
-              />
-            )}
-          </FormField>
-          <FormField initialValue={location.city} name="city">
-            {(formFieldProps: FormFieldChildProps): React.Node => (
-              <TextField
-                disabled={submitting}
-                label="City"
-                {...formFieldProps}
-              />
-            )}
-          </FormField>
-          <FormField initialValue={location.state} name="state">
-            {(formFieldProps: FormFieldChildProps): React.Node => (
-              <PickerField label="State" {...formFieldProps}>
-                {STATE_LIST.map((stateConfig: StateConfig): React.Node => (
-                  <PickerField.Item key={stateConfig.value} {...stateConfig} />
-                ))}
-              </PickerField>
-            )}
-          </FormField>
-          <FormField initialValue={location.zipCode} name="zipCode">
-            {(formFieldProps: FormFieldChildProps): React.Node => (
-              <TextField
-                disabled={submitting}
-                keyboardType="numeric"
-                label="Zip"
-                {...formFieldProps}
-              />
-            )}
-          </FormField>
-          <FormField name="id" initialValue={location.id} />
-          <FormValidationMessage>{formError}</FormValidationMessage>
-          <Button
-            disabled={submitting || invalid || pristine}
-            onPress={(): void => handleSubmit(onSubmit)}
-            title={submitButtonLabel}
-          />
-        </KeyboardAwareScrollView>
+            </KeyboardAwareScrollView>
+          )}
+        </FieldFocusManager>
       )}
     </Form>
   ),
