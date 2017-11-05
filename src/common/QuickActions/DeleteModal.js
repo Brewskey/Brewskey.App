7b00c42052a55a413da80a3bd660b6ebaf9980cb
Anton puko <antonpukowebdev@gmail.com>
2017-11-05 02:10:57 +0200

    implement modal for delete item

---
+++ src/common/QuickActions/DeleteModal.js
@@ -0,0 +1,47 @@
+// @flow
+
+import * as React from 'react';
+import styled from 'styled-components/native';
+import Modal from 'react-native-modal';
+import { Button } from 'react-native-elements';
+
+const Container = styled.View`
+  background-color: white;
+  justify-content: center;
+  padding-bottom: 25;
+  padding-top: 25;
+`;
+
+const Message = styled.Text`
+  align-items: center;
+  margin-bottom: 20;
+  text-align: center;
+`;
+
+type Props = {|
+  isVisible: boolean,
+  message?: string,
+  onCancelButtonPress: () => void,
+  onDeleteButtonPress: () => void,
+|};
+
+const DeleteModal = ({
+  isVisible,
+  message = 'Are you sure you want to delete this item?',
+  onCancelButtonPress,
+  onDeleteButtonPress,
+}: Props): React.Node => (
+  <Modal
+    isVisible={isVisible}
+    onBackButtonPress={onCancelButtonPress}
+    onBackdropPress={onCancelButtonPress}
+  >
+    <Container>
+      <Message>{message}</Message>
+      <Button onPress={onCancelButtonPress} title="cancel" />
+      <Button onPress={onDeleteButtonPress} title="delete" />
+    </Container>
+  </Modal>
+);
+
+export default DeleteModal;
