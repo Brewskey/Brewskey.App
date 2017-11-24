29228557ef14e4e104bf0a28617fe71da4251569
John Kalberer <jlkalberer@gmail.com>
2017-11-24 15:00:16 -0800

    Added modal for NFC. This doesn't actually do anything yet.

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+const theme = {
+  button: {
+    white: {
+      buttonStyle: { backgroundColor: 'white' },
+      textStyle: { color: 'black' },
+    },
+    wide: {
+      buttonStyle: { width: '100%' },
+    },
+  },
+};
+
+export default theme;
