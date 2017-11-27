5c6bdb7b3e8754fd6cb4e55a83834054ac28daa8
Anton puko <antonpukowebdev@gmail.com>
2017-11-27 23:43:55 +0200

    add colors to theme

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,5 @@
+// @flow
+
 const theme = {
   button: {
     white: {
@@ -7,6 +9,17 @@ const theme = {
     wide: {
       buttonStyle: { width: '100%' },
     },
+  },
+  colors: {
+    accent: '#efb534',
+    error: '#f44336',
+    outline: '#eee',
+    primary: '#3c91a1',
+    primary2: '#238393',
+    primary3: '#1e717e',
+    text: '#000',
+    textFaded: '#e3e3e3',
+    textInverse: '#fff',
   },
 };
 
