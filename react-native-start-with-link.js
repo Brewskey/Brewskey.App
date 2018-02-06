8116ced9213460c4e30da47c1d27babb0205d198
Anton puko <antonpukowebdev@gmail.com>
2018-02-06 16:55:21 +0200

    add node-rsa and bunch of modules for make it work in r-n

--- react-native-start-with-link.js
+++ react-native-start-with-link.js
@@ -54,7 +54,15 @@ try {
 
 var config = {
   extraNodeModules: {
-    'react-native': path.resolve(__dirname, 'node_modules/react-native')
+    'assert': require.resolve('assert/'),
+    'buffer': require.resolve('buffer/'),
+    'constants': require.resolve('constants-browserify'),
+    'crypto': require.resolve('react-native-crypto'),
+    'events': require.resolve('events/'),
+    'process': require.resolve('process/browser.js'),
+    'react-native': path.resolve(__dirname, 'node_modules/react-native'),
+    'stream': require.resolve('readable-stream'),
+    'vm': require.resolve('vm-browserify')
   },
   getBlacklistRE() {
     return blacklist([
