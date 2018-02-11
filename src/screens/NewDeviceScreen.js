8f947b79216cbf35e7da5e89bb6bb477aa08096a
Anton puko <antonpukowebdev@gmail.com>
2018-02-11 03:37:35 +0200

    combine edit/new device form into one. fix wifisetup -> deviceform flow

--- src/screens/NewDeviceScreen.js
+++ src/screens/NewDeviceScreen.js
@@ -9,14 +9,17 @@ import { NavigationActions } from 'react-navigation';
 import { observer } from 'mobx-react';
 import DAOApi from 'brewskey.js-api';
 import { DeviceStore, waitForLoaded } from '../stores/DAOStores';
+import flatNavigationParamsAndScreenProps from '../common/flatNavigationParamsAndScreenProps';
 import Container from '../common/Container';
 import Header from '../common/Header';
-import NewDeviceForm from '../components/NewDeviceForm';
+import DeviceForm from '../components/DeviceForm';
 
 type InjectedProps = {|
   navigation: Navigation,
+  particleID: string,
 |};
 
+@flatNavigationParamsAndScreenProps
 @observer
 class NewDeviceScreen extends InjectedComponent<InjectedProps> {
   _onFormSubmit = async (values: DeviceMutator): Promise<void> => {
@@ -38,10 +41,15 @@ class NewDeviceScreen extends InjectedComponent<InjectedProps> {
   };
 
   render() {
+    const { particleID } = this.injectedProps;
     return (
       <Container>
         <Header showBackButton title="New brewskey box" />
-        <NewDeviceForm onSubmit={this._onFormSubmit} />
+        <DeviceForm
+          device={{ particleId: particleID }}
+          onSubmit={this._onFormSubmit}
+          submitButtonLabel="Create Device"
+        />
       </Container>
     );
   }
