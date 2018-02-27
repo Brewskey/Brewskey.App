29d9d4db07ed565b4210ae5bede4dd7a0733b1a9
Anton puko <antonpukowebdev@gmail.com>
2018-02-28 00:14:39 +0200

    implement own Header instead RNEHeader.

--- src/common/Header/index.js
+++ src/common/Header/index.js
@@ -1,59 +1,91 @@
 // @flow
-
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
-import { Header as RNEHeader } from 'react-native-elements';
-import HeaderDrawerButton from './HeaderDrawerButton';
-import HeaderBackButton from './HeaderBackButton';
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
 import { COLORS, TYPOGRAPHY } from '../../theme';
+import HeaderBackButton from './HeaderBackButton';
+import HeaderDrawerButton from './HeaderDrawerButton';
 
+// todo title have slight offset to the left when there are more than one
+// button in the right component. Need to figure better solution to position
+// components inside header, to be able to keep title in the center always.
 const styles = StyleSheet.create({
+  fakeHeaderButton: {
+    height: 31,
+    width: 31,
+  },
   innerContainer: {
     alignItems: 'center',
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   outerContainer: {
     backgroundColor: COLORS.primary2,
-    borderBottomWidth: 0,
     elevation: 2,
     height: 60,
-    paddingVertical: 0,
+    overflow: 'visible',
+    paddingHorizontal: 12,
   },
   title: {
     ...TYPOGRAPHY.heading,
     color: COLORS.textInverse,
   },
+  titleContainer: {
+    alignItems: 'center',
+    flex: 1,
+  },
 });
 
-type Props = {
-  leftComponent?: React.Element<any>,
-  rightComponent?: React.Element<any>,
+const FakeHeaderButton = () => <View style={styles.fakeHeaderButton} />;
+
+type Props = {|
+  leftComponent?: ?React.Element<any> | React.ComponentType<any>,
+  rightComponent?: ?React.Element<any> | React.ComponentType<any>,
   showBackButton?: boolean,
-  title?: string,
-};
+  title?: ?string,
+|};
 
 class Header extends React.PureComponent<Props> {
+  static defaultProps = {
+    leftComponent: <HeaderDrawerButton />,
+    rightComponent: <FakeHeaderButton />,
+  };
+
   render() {
     const { leftComponent, rightComponent, showBackButton, title } = this.props;
+    const LeftComponent = (leftComponent: any);
+    const RightComponent = (rightComponent: any);
 
-    let LeftComponent = <HeaderDrawerButton />;
+    let leftElement;
     if (showBackButton) {
-      LeftComponent = <HeaderBackButton />;
+      leftElement = <HeaderBackButton />;
+    } else {
+      leftElement = React.isValidElement(LeftComponent) ? (
+        LeftComponent
+      ) : (
+        <LeftComponent />
+      );
     }
-    if (leftComponent) {
-      LeftComponent = leftComponent;
-    }
+
+    const rightElement = React.isValidElement(RightComponent) ? (
+      RightComponent
+    ) : (
+      <RightComponent />
+    );
 
     return (
-      <RNEHeader
-        statusBarProps={{
-          backgroundColor: COLORS.primary3,
-        }}
-        centerComponent={{ style: styles.title, text: title }}
-        leftComponent={LeftComponent}
-        innerContainerStyles={styles.innerContainer}
-        outerContainerStyles={styles.outerContainer}
-        rightComponent={rightComponent}
-      />
+      <View style={styles.outerContainer}>
+        <StatusBar backgroundColor={COLORS.primary3} />
+        <View style={styles.innerContainer}>
+          {leftElement}
+          <View style={styles.titleContainer}>
+            <Text numberOfLines={1} style={styles.title}>
+              {title}
+            </Text>
+          </View>
+          {rightElement}
+        </View>
+      </View>
     );
   }
 }
