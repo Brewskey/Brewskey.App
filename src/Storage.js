775f196e913d9bbb40bee6a0b37a0ed79aaf7f06
Anton puko <antonpukowebdev@gmail.com>
2018-03-14 18:39:07 +0200

    sve notifications and app settings by userKey

---
+++ src/Storage.js
@@ -0,0 +1,51 @@
+// @flow
+
+import { AsyncStorage } from 'react-native';
+import { when } from 'mobx';
+import AuthStore from './stores/AuthStore';
+
+class Storage {
+  static set = (key: string, value: Object): Promise<void> =>
+    AsyncStorage.setItem(key, JSON.stringify(value));
+
+  static get = async (key: string): Promise<any> => {
+    const stringValue = await AsyncStorage.getItem(key);
+    return stringValue ? JSON.parse(stringValue) : null;
+  };
+
+  static remove = AsyncStorage.removeItem;
+
+  static setForCurrentUser = async (key: string, value: any): Promise<void> => {
+    const keyForCurrentUser = await Storage._getKeyForCurrentUser(key);
+    await Storage.set(keyForCurrentUser, value);
+  };
+
+  static getForCurrentUser = async (key: string): Promise<any> => {
+    const keyForCurrentUser = await Storage._getKeyForCurrentUser(key);
+    return Storage.get(keyForCurrentUser);
+  };
+
+  static removeForCurrentUser = async (key: string): Promise<void> => {
+    const keyForCurrentUser = await Storage._getKeyForCurrentUser(key);
+    return Storage.remove(keyForCurrentUser);
+  };
+
+  static _getKeyForCurrentUser = async (key: string): Promise<string> => {
+    const userID = await Storage._getUserID();
+    return `${userID}/${key}`;
+  };
+
+  static _getUserID = (): Promise<void> =>
+    new Promise((resolve: () => void) => {
+      if (AuthStore.isAuthorized) {
+        resolve(AuthStore.userID);
+        return;
+      }
+      when(
+        (): boolean => AuthStore.isAuthorized,
+        () => resolve(AuthStore.userID),
+      );
+    });
+}
+
+export default Storage;
