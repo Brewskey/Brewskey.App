473ae5c046d94aa559de06d5adade20a4f0a74fa
Anton puko <antonpukowebdev@gmail.com>
2018-04-19 23:02:16 +0200

    add loading state to avatar picker, fix lint-staged breaking change

--- src/components/AvatarPicker.js
+++ src/components/AvatarPicker.js
@@ -3,13 +3,28 @@
 import type CachedImage from '../common/CachedImage';
 
 import * as React from 'react';
+import { StyleSheet } from 'react-native';
 import nullthrows from 'nullthrows';
 import ImagePicker from 'react-native-image-picker';
 import { observer } from 'mobx-react/native';
+import { computed, observable, runInAction, when } from 'mobx';
 import AuthStore from '../stores/AuthStore';
-import UserAvatar from '../common/avatars/UserAvatar';
+import SnackBarStore from '../stores/SnackBarStore';
 import { UpdateAvatarStore } from '../stores/ApiRequestStores/CommonApiStores';
-import { waitForLoaded } from '../stores/DAOStores';
+import UserAvatar from '../common/avatars/UserAvatar';
+import LoadingIndicator from '../common/LoadingIndicator';
+import { COLORS } from '../theme';
+
+const styles = StyleSheet.create({
+  loadingIndicator: {
+    alignItems: 'center',
+    backgroundColor: COLORS.secondary2,
+    borderRadius: 100,
+    height: 200,
+    justifyContent: 'center',
+    width: 200,
+  },
+});
 
 const IMAGE_PICKER_OPTIONS = {
   title: 'SelectAvatar',
@@ -19,6 +34,15 @@ const IMAGE_PICKER_OPTIONS = {
 class AvatarPicker extends React.Component<{}> {
   _cachedImageRef: ?CachedImage;
 
+  @observable _updateAvatarCacheKey = null;
+
+  @computed
+  get _isLoading(): boolean {
+    return UpdateAvatarStore.getFromCache(
+      this._updateAvatarCacheKey,
+    ).isLoading();
+  }
+
   _onAvatarPress = () => {
     ImagePicker.showImagePicker(
       IMAGE_PICKER_OPTIONS,
@@ -27,8 +51,12 @@ class AvatarPicker extends React.Component<{}> {
           return;
         }
 
-        await waitForLoaded(() => UpdateAvatarStore.get(data));
-        nullthrows(this._cachedImageRef).flushCache();
+        runInAction(async () => {
+          this._updateAvatarCacheKey = UpdateAvatarStore.fetch(data);
+          await when(() => !this._isLoading);
+          nullthrows(this._cachedImageRef).flushCache();
+          SnackBarStore.showMessage({ text: 'Avatar updated' });
+        });
       },
     );
   };
@@ -38,7 +66,9 @@ class AvatarPicker extends React.Component<{}> {
   };
 
   render() {
-    return (
+    return this._isLoading ? (
+      <LoadingIndicator style={styles.loadingIndicator} />
+    ) : (
       <UserAvatar
         imageRef={this._getAvatarImageRef}
         onPress={this._onAvatarPress}
