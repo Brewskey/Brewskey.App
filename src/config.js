3821298a478c45033f6587789560d925327fac49
AntonPuko <stinger_anton@mail.ru>
2018-12-06 23:56:17 +0200

    revert host slash change

--- src/config.js
+++ src/config.js
@@ -1,10 +1,9 @@
 // @flow
 
-const HOST = 'https://brewskey.com/';
-// const HOST = 'http://10.0.2.2:2484/';
+const HOST = 'https://brewskey.com';
+// const HOST = 'http://10.0.2.2:2484';
 
 export default {
-  CDN: `${HOST}cdn/`,
+  CDN: `${HOST}/cdn/`,
   HOST,
-  SIGNALR: `${HOST}signalr/hubs`,
 };
